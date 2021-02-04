3971f9d77f4a975875d363e2867c502dd199aa36
AP <60903271+PrivatPinguin@users.noreply.github.com>
2021-02-04 20:11:18 +0100

    Update CubeAnimation.ps1

--- CubeAnimation.ps1
+++ CubeAnimation.ps1
@@ -71,11 +71,21 @@ $cubeList = (
         +---+--+.  |
           . |     .|
             +------+")
-$n = 200#70
+            
+#millisecondsTimer            
+$mlt = 70
+
+#Remove first and last element to create a loop
 $cubeListReverse = $cubeList
 $cubeListReverse = $cubeList[1..($cubeList.Length-1)]
-#$cubeListReverse = $cubeListReverse.RemoveAt(0)
 [array]::Reverse($cubeListReverse)
+
+# reminder:
+Write-Host "`n`nReminder:: Press " -NoNewline
+Write-Host "[ Strg + C ]" -ForegroundColor red -NoNewline
+Write-Host " to exit loop.`n`nLoop starts in 5 seconds"
+Start-Sleep -Seconds 5
+
 while($True){
 
        
@@ -83,13 +93,13 @@ while($True){
     {
         clear 
         $cube
-        Start-Sleep -Milliseconds $n
+        Start-Sleep -Milliseconds $mlt
     }   
     foreach($cube in $cubeListReverse)
     {
         clear 
         $cube
-        Start-Sleep -Milliseconds $n
+        Start-Sleep -Milliseconds $mlt
     }
 
 
